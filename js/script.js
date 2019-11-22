f54b5caf2e1304e37001e14cec34e7e4873201fe
Hasani Valdez <54682559+Hvaldez2@users.noreply.github.com>
2019-11-21 19:52:35 -0600

    adding functions and updating html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+	var button = document.getElementById('entrybutton');
+	
+	function showMe(){
+		var textbox = document.getElementById('entryinput')
+		document.getElementById('textoutput').innerHTML = textbox.value;
+		alert("Hasani Valdez: " + textbox.value);
 
 
+	}
+	
+	button.addEventListener('click', showMe);
 
 
-
-
+}
 
 window.addEventListener('load', init);
-}
